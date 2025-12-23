2092e145f7b7f7d8671286b4b4b43ae49b69ed8f
adrianmaciuc <adrian.maciuc@gmail.com>
2025-12-23 22:40:56 +0200

    cloudinary plugin

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,1 +1,21 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: env("UPLOAD_PROVIDER", "local"),
+      providerOptions:
+        env("UPLOAD_PROVIDER", "local") === "cloudinary"
+          ? {
+              cloud_name: env("CLOUDINARY_NAME"),
+              api_key: env("CLOUDINARY_KEY"),
+              api_secret: env("CLOUDINARY_SECRET"),
+            }
+          : {},
+      actionOptions: {
+        upload: {
+          folder: env("CLOUDINARY_FOLDER", "recipes"),
+        },
+        delete: {},
+      },
+    },
+  },
+});
